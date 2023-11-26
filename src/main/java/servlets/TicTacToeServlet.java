4e18d3d4b7547db2093cc138567edc889d685805
Brandon <brandonnolan20@gmail.com>
2023-11-26 14:26:54 -0500

    Add updatded tictactoe table

--- src/main/java/servlets/TicTacToeServlet.java
+++ src/main/java/servlets/TicTacToeServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import beans.Game;
 import beans.User;
 import dao.UserDao;
 import multithreading.ScoreExecutorService;
@@ -38,7 +37,7 @@ public class TicTacToeServlet extends HttpServlet {
 		// Temporary, need to change this in 3rd assignment to automatically know the
 		// teacher based on user currently logged in
 		if (action == null) {
-			Game newGame = new Game();
+
 			writer.write(writeTicTacToeGame() + "<br>");
 
 			if (errorMessage != null) {
@@ -85,10 +84,27 @@ public class TicTacToeServlet extends HttpServlet {
 
 	// temporary code to create a "Tic Tac Toe" page
 	public String writeTicTacToeGame() {
-		String form = "<!doctype html><html><title>Tic Tac Toe!</title>"
-				+ "<body><h1>Placeholder for Tic Tac Toe game</h1>"
-
-				+ "<form method='post' action='tictactoeservlet'"
+		String form = "<!doctype html><html><title>Tic Tac Toe!</title>" + "<head>\r\n"
+				+ "  <meta charset=\"UTF-8\">\r\n"
+				+ "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + "  <style>\r\n"
+				+ "    table {\r\n" + "      border-collapse: collapse;\r\n" + "      width: 300px;\r\n"
+				+ "      margin: 50px auto;\r\n" + "    }\r\n" + "\r\n" + " th, td {\r\n"
+				+ "      border: 1px solid #ddd;\r\n" + "      text-align: center;\r\n"
+				+ "      width: 100px; /* Set a fixed width for each cell */\r\n"
+				+ "      height:100px; /* Set a fixed height for each cell */\r\n" + "    }" + "    input.game {\r\n"
+				+ "      width: 100%;\r\n" + "      height: 100%;\r\n" + "      box-sizing: border-box;"
+				+ "		 font-size: 50px;" + " 	text-align: center;" + "    }\r\n" + "  </style>\r\n" + "</head>"
+				+ "<body><h1>Placeholder for Tic Tac Toe game</h1>" + "<table>\r\n" + "  <tr>\r\n"
+				+ "    <td><input class = \"game\"  type=\"text\" class=\"editable-cell\"></td>\r\n"
+				+ "    <td><input class = \"game\" type=\"text\" class=\"editable-cell\"></td>\r\n"
+				+ "    <td><input class = \"game\" type=\"text\" class=\"editable-cell\"></td>\r\n" + "  </tr>\r\n"
+				+ "  <tr>\r\n" + "    <td><input class = \"game\" type=\"text\" class=\"editable-cell\"></td>\r\n"
+				+ "    <td><input class = \"game\" type=\"text\" class=\"editable-cell\"></td>\r\n"
+				+ "    <td><input class = \"game\" type=\"text\" class=\"editable-cell\"></td>\r\n" + "  </tr>\r\n"
+				+ "  <tr>\r\n" + "    <td><input class = \"game\" type=\"text\" class=\"editable-cell\"></td>\r\n"
+				+ "    <td><input class = \"game\" type=\"text\" class=\"editable-cell\"></td>\r\n"
+				+ "    <td><input class = \"game\" type=\"text\" class=\"editable-cell\"></td>\r\n" + "  </tr>\r\n"
+				+ "</table>" + "<form method='post' action='tictactoeservlet'"
 				+ "<table><tr><td><label>Please enter student number of the winner</label><input type='number' name='studentNumber' required='required'</td></tr><br><br>"
 				+ "<tr><td><input type='submit' name='action' value='Submit'</td></tr>" + "</table></form>"
 
